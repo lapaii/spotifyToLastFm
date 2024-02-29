7d4d7b7a9d7108b661b030557c085cb726119ab7
lapaii <lapaii@lapaii.dev>
2024-02-29 17:03:17 +0000

    why did i put semicolons

--- scrobble.py
+++ scrobble.py
@@ -2,8 +2,8 @@ import pylast, json, os, time
 
 LASTFM_API_KEY = os.environ.get('API_KEY')
 LASTFM_API_SECRET = os.environ.get('API_SECRET')
-LASTFM_USERNAME = os.environ.get('USERNAME');
-LASTFM_PASSWORD = pylast.md5(os.environ.get('PASSWORD'));
+LASTFM_USERNAME = os.environ.get('USERNAME')
+LASTFM_PASSWORD = pylast.md5(os.environ.get('PASSWORD'))
 
 network = pylast.LastFMNetwork(api_key=LASTFM_API_KEY, api_secret=LASTFM_API_SECRET, username=LASTFM_USERNAME, password_hash=LASTFM_PASSWORD)
 
